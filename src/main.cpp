7f1412119a2474539c33a87003d59c0109db5470
SebL <sebastien.levy@mentor.com>
2018-10-19 11:15:27 +0200

    cmake experiments
    - 2 targets, an executable, a library
    - static linking

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
+#include "plugins/message.hpp"
 
 int main( int, char const *[] )
 {
   std::cout << "hello warp-native\n";
+  std::cout << "-----------------\n";
+
+  std::cout << message{ "How are you doing?\n" };
 }
